772a52e8dbe5d2b87280253b093029728f3620f3
张海生 <zhaisheng@rolaner.com>
2017-05-31 19:57:31 +0800

    增加静态数据 写入 es

--- src/Srt/Srt.php
+++ src/Srt/Srt.php
@@ -6,11 +6,15 @@
  * Time: 18:46
  */
 namespace Zimu\Srt;
+
+use Elasticsearch\ClientBuilder;
+
 class Srt implements SrtInterface
 {
     const SRT_DIR = 'srts';
     public $sirs = '';
     public $sirs_unsaved = '';
+    public $esClient;
     
     public function __construct()
     {
@@ -60,8 +64,19 @@ class Srt implements SrtInterface
     
     public function hi(){
         echo 'hi';
-        $this->initPaths();
+        $this->init();
         $this->gets();
+        $this->addIndex();
+    }
+    
+    //保存进 es
+    public function save(){
+    
+    }
+    
+    public function init(){
+        $this->initPaths();
+        $this->initEs();
     }
     
     public function initPaths(){
@@ -69,4 +84,38 @@ class Srt implements SrtInterface
         echo $this->sirs;
         $this->sirs_unsaved = $this->sirs.'/unsaved';
     }
+    public function initEs(){
+        $this->esClient = ClientBuilder::create()->build();
+    }
+    
+    //建立索引 http://localhost:9200/zimu/subhd/335407
+    public function addIndex(){
+        $params = [
+            'index' => 'zimu',
+            'type' => 'subhd',
+            'id' => '335407',
+            'body' => [
+                'z_id' => 'subhd_335407',
+                'name'=>'求婚大作战',
+                'role'=>'山下智久,长泽雅美,藤木直人,荣仓奈奈,平冈佑太,滨田岳,松重丰',
+                'category' =>'剧情,爱情,奇幻 ',
+                'title'=>'[ドラマ][プロポーズ大作戦 第01話]「甲子園行けたら結婚できる！？」',
+                'srts'=>[
+                    [
+                        't'=>'00:00:03,937 --> 00:00:11,444',
+                        'words'=>'说出你的愿望吧'
+                    ],
+                    [
+                        't'=>'00:00:03,937 --> 00:00:11,444',
+                        'words'=>"努力试着去改变过去
+                                那个懦弱的自己"
+                    ]
+                ]
+            ]
+        ];
+        
+        $response = $this->esClient->index($params);
+        return $response;
+    }
+    
 }
